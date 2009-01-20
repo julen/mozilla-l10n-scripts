237a9157ec78ad954477cf1d14be649e38d5cd27
Julen Ruiz Aizpuru <julenx@gmail.com>
2009-01-20 17:56:14 +0100

    Edit exclude filters. It seems it didn't work as expected before.

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ PRODUCTS = ["TB"]
 REPO = "1.9.1"
 
 # Parameters passed to moz2po/po2moz scripts
-COMMON_PARAMS = ["--errorlevel=traceback", "--progress=verbose", "-x *.xhtml", "-x *.inc", "-x *.xml", "-x *.rdf", "-x bookmarks.html", "-x welcome.xhtml", "-x platformKeys.properties", "-x pref-publish.dtd", "-x *.txt", "-x *.css", "-x charset.mk"]
+COMMON_PARAMS = ["--errorlevel=traceback", "--progress=verbose", "--exclude=*.xhtml", "--exclude=*.inc", "--exclude=*.xml" "--exclude=*.rdf", "--exclude=*.js", "--exclude=*.html", "--exclude=*.txt", "--exclude=*.css", "--exclude=*.mk"]
 MOZ2PO_PARAMS = ["--duplicates=msgctxt"] + COMMON_PARAMS[:]
 PO2MOZ_PARAMS = COMMON_PARAMS[:]
 
